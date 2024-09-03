af82adb560b890fbe536c37dae9b49c5dbc3f3f7
Layla Techno <154958698+laylatechno@users.noreply.github.com>
2024-09-03 16:20:23 +0700

    slider

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -66,7 +66,7 @@ class HomeController extends Controller
 
         $alasan = Alasan::all();
         $testimoni = Testimoni::all();
-        $slider = Slider::all();
+        $slider = Slider::orderBy('urutan', 'asc');
         $kategori_produk = KategoriProduk::all();
         // Ambil kategori dengan urutan pertama yang tersedia
         $kategori_pertama = KategoriProduk::orderBy('urutan', 'asc')
